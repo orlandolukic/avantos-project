11aa9091575fd2dabc002bcbf11a6bddfdfbf7ff
Luka Orlandić <orlandolukic96@gmail.com>
2026-03-14 10:00:04 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@xyflow/react/dist/style.css';
+import {addEdge, applyEdgeChanges, applyNodeChanges, ReactFlow} from "@xyflow/react";
+
+const initialNodes = [
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
 
 function App() {
+
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+      (changes: any) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+      [],
+  );
+  const onEdgesChange = useCallback(
+      (changes: any) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+      [],
+  );
+  const onConnect = useCallback(
+      (params: any) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+      [],
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            fitView
+        />
+      </div>
     </div>
   );
 }
